88095601301117c196b00b092dfcd7ad7e1a414e
Oleksii Leshchenko <84497187+alexbreamdev@users.noreply.github.com>
2023-04-26 22:03:40 +0300

    colours, icons and symbols setup

---
+++ ThePeople/ThePeople/Resources/Symbols.swift
@@ -0,0 +1,15 @@
+//
+//  Symbols.swift
+//  ThePeople
+//
+//  Created by Oleksii Leshchenko on 26.04.2023.
+//
+
+import SwiftUI
+// 1. enum can't be initialised
+// 2. all symbols are in one place
+enum Symbols {
+    static let person = Image(systemName: "person.2")
+    static let gear = Image(systemName: "gear")
+    static let plus = Image(systemName: "plus")
+}
---
+++ ThePeople/ThePeople/Resources/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  ThePeople
+//
+//  Created by Oleksii Leshchenko on 26.04.2023.
+//
+
+import SwiftUI
+
+enum Theme {
+    static let background = Color("background")
+    static let detailBackground = Color("detail-background")
+    static let text = Color("text")
+    static let pill = Color("pill")
+}
