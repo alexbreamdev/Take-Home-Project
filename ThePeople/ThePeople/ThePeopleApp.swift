1ee662fd611e505e21bab6b3a756b96f1e17a993
Oleksii Leshchenko <84497187+alexbreamdev@users.noreply.github.com>
2023-04-26 18:36:59 +0300

    project created

---
+++ ThePeople/ThePeople/ThePeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThePeopleApp.swift
+//  ThePeople
+//
+//  Created by Oleksii Leshchenko on 26.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ThePeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
