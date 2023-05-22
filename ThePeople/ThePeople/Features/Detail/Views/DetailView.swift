0f1521ab08ab9a5bc360b73b5eef54848fab4cd8
Oleksii Leshchenko <84497187+alexbreamdev@users.noreply.github.com>
2023-05-22 21:09:21 +0300

    isLoading functionality of detailView

--- ThePeople/ThePeople/Features/Detail/Views/DetailView.swift
+++ ThePeople/ThePeople/Features/Detail/Views/DetailView.swift
@@ -13,19 +13,23 @@ struct DetailView: View {
     var body: some View {
         ZStack {
             background
-            ScrollView {
-                VStack(alignment: .leading, spacing: 18) {
-                    avatar
-                    
-                    Group {
-                        general
-                        link
+            if detailViewModel.isLoading {
+                ProgressView()
+            } else {
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 18) {
+                        avatar
+                        
+                        Group {
+                            general
+                            link
+                        }
+                        .padding(.horizontal, 8)
+                        .padding(.vertical, 18)
+                        .background(Theme.detailBackground, in: RoundedRectangle(cornerRadius: 16, style: .continuous))
                     }
-                    .padding(.horizontal, 8)
-                    .padding(.vertical, 18)
-                    .background(Theme.detailBackground, in: RoundedRectangle(cornerRadius: 16, style: .continuous))
+                    .padding()
                 }
-                .padding()
             }
         }
         .navigationTitle("Details")
--- ThePeople/ThePeople/Features/Detail/Views/DetailViewModel.swift
+++ ThePeople/ThePeople/Features/Detail/Views/DetailViewModel.swift
@@ -11,10 +11,15 @@ final class DetailViewModel: ObservableObject {
     @Published private(set) var user: User?
     @Published private(set) var support: Support?
     @Published private(set) var error: NetworkingManager.NetworkingError?
+    @Published private(set) var isLoading: Bool = false
     @Published var hasError = false
     
     func fetchDetails(for userId: Int) {
+        isLoading = true
         NetworkingManager.shared.request("https://reqres.in/api/users/\(userId)", type: UserDetailResponse.self) {[weak self] result in
+            defer {
+                self?.isLoading = false
+            }
             switch result {
             case .success(let response):
                 DispatchQueue.main.async {
